b0f8d3436dde1a9a84f404ced7a6570ca3dd046e
= <=>
2021-03-02 19:08:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/superacaobikes/superacaobikes/SuperacaobikesApplication.java
@@ -0,0 +1,13 @@
+package br.com.superacaobikes.superacaobikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperacaobikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperacaobikesApplication.class, args);
+	}
+
+}
